2c3c197063d17dfe12b34927f7363fa46100e930
Huang Xiaobin <xiaobin.huang@gmail.com>
2018-04-28 02:23:05 +0800

    move networking.base here

---
+++ _Script/Networking/IChanModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace x600d1dea.networking
+{
+	public interface IChanModule : IDisposable 
+	{
+		// main thread
+		void Init(Chan chan);
+
+		// worker thread
+		void ProcessSendingBytes(ref byte[] data);
+		// worker thread
+		void ProcessReceivedBytes(ref byte[] data);
+	}
+}
