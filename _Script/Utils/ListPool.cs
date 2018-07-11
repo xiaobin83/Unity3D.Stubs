3852c75b696a19cab3e61f85bb3d12dfc1f62ed8
xiaobin83 <xiaobin.huang@gmail.com>
2018-07-11 18:44:42 +0800

    using asmdef

---
+++ _Script/Utils/ListPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace x600d1dea.stubs.utils
+{
+	public static class ListPool<T>
+	{
+		static List<List<T>> pool = new List<List<T>>();
+		public static List<T> Get()
+		{
+			if (pool.Count > 0)
+			{
+				var r = pool[pool.Count - 1];
+				pool.RemoveAt(pool.Count - 1);
+				return r;
+			}
+			else
+			{
+				return new List<T>();
+			}
+		}
+		public static void Release(List<T> list)
+		{
+			list.Clear();
+			pool.Add(list);
+		}
+
+		public static void Clear()
+		{
+			pool.Clear();
+		}
+	}
+}
