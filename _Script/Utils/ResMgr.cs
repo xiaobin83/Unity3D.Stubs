f1596f733ec5fadee26ba1a12337f9d28a4e35fb
xiaobin83 <xiaobin.huang@gmail.com>
2018-07-24 17:33:55 +0800

    add LoadCustom*

--- _Script/Utils/ResMgr.cs
+++ _Script/Utils/ResMgr.cs
@@ -18,6 +18,53 @@ namespace x600d1dea.stubs.utils
 				}
 				return null;
 			}
+
+			public virtual string LoadText(string path)
+			{
+				var asset = Resources.Load<TextAsset>(path);
+				if (asset != null)
+				{
+					return asset.text;
+				}
+				return null;
+			}
+
+			public virtual byte[] LoadEncryptedBytes(string path)
+			{
+				var bytes = LoadBytes(path);
+				if (bytes != null)
+				{
+					if (pike != null)
+					{
+						pike.Codec(ref bytes);
+						return bytes;
+					}
+					Debug.LogError("load encrypted data without crypto key");
+					return null;
+				}
+				return null;
+			}
+
+			public virtual string LoadCustomText(string tag, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public virtual byte[] LoadCustomBytes(string tag, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public virtual string LoadEncryptedText(string path)
+			{
+				var bytes = LoadEncryptedBytes(path);
+				if (bytes != null)
+				{
+					return System.Text.Encoding.UTF8.GetString(bytes);
+				}
+				return null;
+			}
+
 			public virtual T Load<T>(string path) where T: Object
 			{
 				return Resources.Load<T>(path);
@@ -58,6 +105,21 @@ namespace x600d1dea.stubs.utils
 				return Resources.LoadAsync(path);
 			}
 
+			public virtual void LoadCustom(string tag, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public virtual Object LoadCustomObject(string tag, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public virtual Object[] LoadAllCustomObjects(string tag, string path)
+			{
+				throw new System.NotImplementedException();
+			}
+
 			public virtual ResourceRequest LoadAsync(string path, System.Type type)
 			{
 				return Resources.LoadAsync(path, type);
@@ -81,18 +143,7 @@ namespace x600d1dea.stubs.utils
 		{
 			if (encrypted)
 			{
-				var bytes = rw.LoadBytes(path);
-				if (bytes != null)
-				{
-					if (pike != null)
-					{
-						pike.Codec(ref bytes);
-						return bytes;
-					}
-					Debug.LogError("load encrypted data without crypto key");
-					return null;
-				}
-				return null;
+				return rw.LoadEncryptedBytes(path);
 			}
 			return rw.LoadBytes(path);
 		}
@@ -101,27 +152,19 @@ namespace x600d1dea.stubs.utils
 		{
 			if (encrypted)
 			{
-				var bytes = rw.LoadBytes(path);
-				if (bytes != null)
-				{
-					if (pike != null)
-					{
-						pike.Codec(ref bytes);
-						return System.Text.Encoding.UTF8.GetString(bytes);
-					}
-					Debug.LogError("load encrypted data wihtout crypto key");
-					return null;
-				}
+				return rw.LoadEncryptedText(path);
 			}
-			else
-			{
-				var asset = rw.Load<TextAsset>(path);
-				if (asset != null)
-				{
-					return asset.text;
-				}
-			}
-			return null;
+			return rw.LoadText(path);
+		}
+
+		public static string LoadCustomText(string tag, string path)
+		{
+			return rw.LoadCustomText(tag, path);
+		}
+
+		public static byte[] LoadCustomBytes(string tag, string path)
+		{
+			return rw.LoadCustomBytes(tag, path);
 		}
 
 		public static Object Load(string uri)
@@ -154,6 +197,22 @@ namespace x600d1dea.stubs.utils
 		{
 			return rw.LoadAll(uri, type);
 		}
+
+		public static void LoadCustom(string tag, string path)
+		{
+			rw.LoadCustom(tag, path);
+		}
+
+		public static Object LoadCustomObject(string tag, string path)
+		{
+			return rw.LoadCustomObject(tag, path);
+		}
+
+		public static Object[] LoadAllCustomObjects(string tag, string path)
+		{
+			return rw.LoadAllCustomObjects(tag, path);
+		}
+
 
 		static IEnumerator AsyncLoader(ResourceRequest r, System.Action<int, Object> progress)
 		{
