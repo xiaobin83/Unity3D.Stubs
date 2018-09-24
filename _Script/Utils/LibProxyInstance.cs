9299c4bf2b307b9ef473090d93e669e97701e7d4
Xiaobin <xiaobin.huang@gmail.com>
2018-09-25 00:44:21 +0800

    add VoiceEvalEngine

---
+++ _Script/Utils/LibProxyInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace x600d1dea.stubs.utils
+{
+	public class LibProxyInstance
+	{
+		public virtual IADSProvider GetADSProvider()
+		{
+			return null;
+		}
+
+		public virtual IVoiceEvalProvider GetVoiceEvalProvider()
+		{
+			return null;
+		}
+	}
+}
+
---
+++ _Script/Utils/NativeCallback.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace x600d1dea.stubs.utils
+{
+	public class NativeCallback : MonoBehaviour
+	{
+		struct CallbackMessage
+		{
+			public string message;
+			public string content;
+		}
+
+		public static NativeCallback Create(string name)
+		{
+			var guid = System.Guid.NewGuid();
+			var callbackObjectName = "_NativeCallback_" + name + "_" + guid.ToString();
+			var go = new GameObject(callbackObjectName);
+			var ncb = go.AddComponent<NativeCallback>();
+			ncb.callbackObjectName = callbackObjectName;
+			return ncb;
+		}
+
+		string callbackObjectName;
+		Dictionary<string, System.Action<string>> callbacks = new Dictionary<string, System.Action<string>>();
+
+		public string New(string name, System.Action<string> callbackDelegate)
+		{
+			var guid = System.Guid.NewGuid();
+			var callbackName = name + "_" + guid.ToString();
+			callbacks.Add(callbackName, callbackDelegate);
+			var proto = new Dictionary<string, string>();
+			proto.Add("name", callbackObjectName);
+			proto.Add("message", callbackName);
+			return JsonConvert.SerializeObject(proto);
+		}
+
+
+		/*
+			message,
+			{
+				message: string,
+				content: string
+			}
+		 */
+		public void DispatchCallback(string jsonString)
+		{
+			var msg = JsonConvert.DeserializeObject<CallbackMessage>(jsonString);
+			System.Action<string> callback;
+			if (callbacks.TryGetValue(msg.message, out callback))
+			{
+				callback(msg.content);
+			}
+			else
+			{
+				Debug.LogErrorFormat("unable to process message: {0}", jsonString);
+			}
+		}
+
+	}
+}
+
